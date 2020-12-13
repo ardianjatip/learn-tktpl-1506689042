0ff89928a08e56ed5959ba6b89c4f19bde64b9a6
ardianjatip <ardian.jati@ui.ac.id>
2020-12-13 17:41:28 +0700

    install and configure cmake

--- HelloWorld/app/src/main/java/com/mobileprogramming/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/mobileprogramming/helloworld/MainActivity.java
@@ -3,13 +3,23 @@ package com.mobileprogramming.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv.setText(meth());
+    }
+
+    protected static String meth(){
+        System.loadLibrary("cpp_code");
     }
 
 }
